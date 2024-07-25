cff00fcf138442824e07f9a43c62bca052ef8935
bypulung <106810287+bypulung@users.noreply.github.com>
2024-07-25 07:17:11 +0700

    Update index.php
    Signed-off-by: bypulung <106810287+bypulung@users.noreply.github.com>

--- index.php
+++ index.php
@@ -2,3 +2,11 @@
 header("Location: https://tanjungbulan.my.id/lomba17", true, 301);
 exit();
 ?>
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+
+    <meta http-equiv="refresh" content="0; url=/lomba17">
+</head>
+</html>
