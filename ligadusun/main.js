87a63016618034a9be84a8ddcc4db3bb7dc77054
bypulung <106810287+bypulung@users.noreply.github.com>
2025-07-23 00:58:47 +0700

    Update main.js
    Signed-off-by: bypulung <106810287+bypulung@users.noreply.github.com>

--- ligadusun/main.js
+++ ligadusun/main.js
@@ -4,5 +4,5 @@ const matches = [
   { home: "Tim B", away: "Tim C", setHome: 1, setAway: 2 },
   { home: "Tim D", away: "Tim A", setHome: 0, setAway: 2 },
   { home: "Tim A", away: "Tim C", setHome: 2, setAway: 0 },
-  { home: "Tim B", away: "Tim D", setHome: 2, setAway: 1 }
+  //{ home: "Tim B", away: "Tim D", setHome: 2, setAway: 1 }
 ];
