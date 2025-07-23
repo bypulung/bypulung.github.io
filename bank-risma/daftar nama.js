3660d36b2f02f4d101a33f815540877f5fc36938
bypulung <106810287+bypulung@users.noreply.github.com>
2025-07-24 00:31:34 +0700

    Create daftar nama.js
    Signed-off-by: bypulung <106810287+bypulung@users.noreply.github.com>

---
+++ bank-risma/daftar nama.js
@@ -0,0 +1,25 @@
+const daftarnama = {
+  putra: [
+    "Tama",
+    "Pulung",
+    "Faisol",
+    "Rifky",
+    "Riswadi",
+    "Mulyono",
+    "Wahyu",
+    "Rifai",
+    "Tio",
+    "Ibnu"
+    "Sogi"
+  ],
+  putri: [
+    "Sasa",
+    "Putri",
+    "Syifa",
+    "Nadia",
+    "Nabila",
+    "Yuyun",
+    "Anis",
+    "Irma"
+  ]
+};
