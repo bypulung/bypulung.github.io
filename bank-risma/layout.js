8b689996c3e7d2063c0eb8a853a00eb38a40fbae
bypulung <106810287+bypulung@users.noreply.github.com>
2025-07-28 11:24:18 +0700

    Update layout.js
    Signed-off-by: bypulung <106810287+bypulung@users.noreply.github.com>

--- bank-risma/layout.js
+++ bank-risma/layout.js
@@ -1,13 +1,5 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Load Header
-  fetch("/bank-risma/header.html")
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("header-placeholder").innerHTML = data;
-    })
-    .catch(err => console.error("Gagal memuat header:", err));
-
   // Load Footer
   fetch("/bank-risma/footer.html")
     .then(res => res.text())
