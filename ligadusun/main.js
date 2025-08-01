ac647dedd2030737106971404c9b31189840efd7
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-01 23:28:11 +0700

    Update main.js

--- ligadusun/main.js
+++ ligadusun/main.js
@@ -1,8 +1,8 @@
 const matches = [
-  { home: "Tim A", away: "Tim B", setHome: 2, setAway: 1 },
-  { home: "Tim C", away: "Tim D", setHome: 2, setAway: 0 },
-  { home: "Tim B", away: "Tim C", setHome: 1, setAway: 2 },
-  { home: "Tim D", away: "Tim A", setHome: 0, setAway: 2 },
-  { home: "Tim A", away: "Tim C", setHome: 2, setAway: 0 },
+  //{ home: "Tim A", away: "Tim B", setHome: 2, setAway: 1 },
+  //{ home: "Tim C", away: "Tim D", setHome: 2, setAway: 0 },
+ // { home: "Tim B", away: "Tim C", setHome: 1, setAway: 2 },
+ // { home: "Tim D", away: "Tim A", setHome: 0, setAway: 2 },
+  //{ home: "Tim A", away: "Tim C", setHome: 2, setAway: 0 },
   //{ home: "Tim B", away: "Tim D", setHome: 2, setAway: 1 }
 ];
