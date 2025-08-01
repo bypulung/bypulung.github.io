824d1dfd0b43611b428392c518e3d5df097d2cad
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-02 01:04:05 +0700

    Update main.js

--- ligadusun/main.js
+++ ligadusun/main.js
@@ -1,8 +1,19 @@
 // Contoh matches (bisa diletakkan di main.js)
 const matches = [
-  // Format: { home, away, setHome, setAway }
-//  { home: "Tim A", away: "Tim B", setHome: 2, setAway: 1 }, // A menang 2-1
-//  { home: "Tim C", away: "Tim D", setHome: 0, setAway: 2 }, // D menang 2-0
-//  { home: "Tim A", away: "Tim C", setHome: 2, setAway: 0 }, // A menang 2-0
-//  { home: "Tim B", away: "Tim D", setHome: 1, setAway: 2 }  // D menang 2-1
+//  Format: { home, away, setHome, setAway }
+  { home: "Tim A", away: "Tim B" }
+  { home: "Tim C", away: "Tim D" }
+  { home: "Tim A", away: "Tim C" }
+  { home: "Tim B", away: "Tim D" }
+  { home: "Tim A", away: "Tim D" }
+  { home: "Tim B", away: "Tim C" }
+
+
+
+//  { home: "Tim A", away: "Tim B", setHome: , setAway:  },
+//  { home: "Tim C", away: "Tim D", setHome: , setAway:  },
+//  { home: "Tim A", away: "Tim C", setHome: , setAway:  },
+//  { home: "Tim B", away: "Tim D", setHome: , setAway:  } 
+
+
 ];
