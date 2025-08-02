f18454b5252aa145afc97bd464ea88b9ec53919c
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-02 23:52:21 +0700

    Update main.js

--- ligadusun/main.js
+++ ligadusun/main.js
@@ -1,7 +1,7 @@
 // Contoh matches (bisa diletakkan di main.js)
 const matches = [
 //  Format: { home, away, setHome, setAway }
-  { home: "Tim A", away: "Tim B" },
+  { home: "Tim A", away: "Tim B", setHome: 2, setAway: 1 },
   { home: "Tim C", away: "Tim D" },
   { home: "Tim A", away: "Tim C" },
   { home: "Tim B", away: "Tim D" },
