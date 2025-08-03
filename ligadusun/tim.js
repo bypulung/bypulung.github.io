4fd2f4567211d1bc03685ed9dbf04de5721614a9
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-03 18:01:41 +0700

    Update tim.js

--- ligadusun/tim.js
+++ ligadusun/tim.js
@@ -1,22 +1,64 @@
 // tim.js
-// Daftar tim dan pemainnya
 const teams = [
   {
-    nama: "Tim A",
-    pemain: ["Andi", "Budi", "Citra"]
+    nama: "Banteng Gaspol",
+    pemain: [
+      "Lek Mitro",
+      "Faisol",
+      "Pulung",
+      "Pak Carto",
+      "Rifky",
+      "Rahayu",
+      "Bu Ida",
+      "RT Ucok",
+      "Bu Linda",
+      "Sogi"
+    ]
   },
   {
-    nama: "Tim B",
-    pemain: ["Dewi", "Eka", "Fajar"]
+    nama: "Merdeka Ngacir",
+    pemain: [
+      "Pak Heri",
+      "Ris",
+      "Cak Topeng",
+      "Pak Asep",
+      "Ripai",
+      "Bu Dini",
+      "Bu Eli",
+      "Lek Nur",
+      "Sasa",
+      "Rehan"
+    ]
   },
   {
-    nama: "Tim C",
-    pemain: ["Galih", "Hani", "Iwan"]
+    nama: "Patriot Ngamuk",
+    pemain: [
+      "Pak Nurman",
+      "Tama",
+      "Pak Bobby",
+      "Mas Iyek",
+      "Tio",
+      "Supreh",
+      "Bu Yunani",
+      "Tukul",
+      "Bu Erna",
+      "Arka"
+    ]
   },
   {
-    nama: "Tim D",
-    pemain: ["Joko", "Kiki", "Lina"]
+    nama: "Pejuang Santuy",
+    pemain: [
+      "RT Yudi",
+      "Mul",
+      "Wahyu",
+      "Pak Supri",
+      "Risky",
+      "Bu Erni",
+      "Putri",
+      "RT Suhdi",
+      "Syifa",
+      "Paman Abu"
+    ]
   }
 ];
-
 window.teams = teams;
