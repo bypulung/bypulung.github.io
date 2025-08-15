4e361ead09b1efaecbff9733847a40e47a1706c4
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-15 23:39:06 +0700

    Create tim.js
    Signed-off-by: bypulung <106810287+bypulung@users.noreply.github.com>

---
+++ tanjungcup/tim.js
@@ -0,0 +1,64 @@
+// tim.js
+const teams = [
+  {
+    nama: "Banteng Gaspol",
+    pemain: [
+      "Lek Mitro",
+      "Faisol",
+      "Pulung",
+      "Pak Carto",
+      "Rifky",
+      "Rahayu",
+      "Bu Ida",
+      "RT Ucok",
+      "Bu Linda",
+      "Sogi"
+    ]
+  },
+  {
+    nama: "Merdeka Ngacir",
+    pemain: [
+      "Pak Heri",
+      "Ris",
+      "Cak Topeng",
+      "Pak Asep",
+      "Ripai",
+      "Bu Dini",
+      "Bu Eli",
+      "Lek Nur",
+      "Sasa",
+      "Rehan"
+    ]
+  },
+  {
+    nama: "Patriot Ngamuk",
+    pemain: [
+      "Pak Nurman",
+      "Tama",
+      "Pak Bobby",
+      "Mas Iyek",
+      "Tio",
+      "Supreh",
+      "Bu Yunani",
+      "Tukul",
+      "Bu Erna",
+      "Arka"
+    ]
+  },
+  {
+    nama: "Pejuang Santuy",
+    pemain: [
+      "RT Yudi",
+      "Mul",
+      "Wahyu",
+      "Pak Supri",
+      "Risky",
+      "Bu Erni",
+      "Putri",
+      "RT Suhdi",
+      "Syifa",
+      "Paman Abu"
+    ]
+  }
+];
+window.teams = teams;
