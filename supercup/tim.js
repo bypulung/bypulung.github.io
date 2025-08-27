be95fba9bb818829e97aa601c6401acc19716ac6
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-28 01:11:35 +0700

    Update tim.js

--- supercup/tim.js
+++ supercup/tim.js
@@ -1,64 +1,95 @@
 // tim.js
 const teams = [
   {
-    nama: "Banteng Gaspol",
+    nama: "Tanjung Bulan A",
     pemain: [
-      "Lek Mitro",
-      "Faisol",
-      "Pulung",
-      "Pak Carto",
-      "Rifky",
-      "Rahayu",
-      "Bu Ida",
-      "RT Ucok",
-      "Bu Linda",
-      "Sogi"
+      "Pemain A1",
+      "Pemain A2",
+      "Pemain A3",
+      "Pemain A4",
+      "Pemain A5",
+      "Pemain A6",
+      "Pemain A7",
+      "Pemain A8",
+      "Pemain A9",
+      "Pemain A10"
     ]
   },
   {
-    nama: "Merdeka Ngacir",
+    nama: "Tanjung Sari 1A",
     pemain: [
-      "Pak Heri",
-      "Ris",
-      "Cak Topeng",
-      "Pak Asep",
-      "Ripai",
-      "Bu Dini",
-      "Bu Eli",
-      "Lek Nur",
-      "Sasa",
-      "Rehan"
+      "Pemain 1A1",
+      "Pemain 1A2",
+      "Pemain 1A3",
+      "Pemain 1A4",
+      "Pemain 1A5",
+      "Pemain 1A6",
+      "Pemain 1A7",
+      "Pemain 1A8",
+      "Pemain 1A9",
+      "Pemain 1A10"
     ]
   },
   {
-    nama: "Patriot Ngamuk",
+    nama: "Tanjung Sari 2A",
     pemain: [
-      "Pak Nurman",
-      "Tama",
-      "Pak Bobby",
-      "Mas Iyek",
-      "Tio",
-      "Supreh",
-      "Bu Yunani",
-      "Tukul",
-      "Bu Erna",
-      "Arka"
+      "Pemain 2A1",
+      "Pemain 2A2",
+      "Pemain 2A3",
+      "Pemain 2A4",
+      "Pemain 2A5",
+      "Pemain 2A6",
+      "Pemain 2A7",
+      "Pemain 2A8",
+      "Pemain 2A9",
+      "Pemain 2A10"
     ]
   },
   {
-    nama: "Pejuang Santuy",
+    nama: "Tanjung Bulan B",
     pemain: [
-      "RT Yudi",
-      "Mul",
-      "Wahyu",
-      "Pak Supri",
-      "Risky",
-      "Bu Erni",
-      "Putri",
-      "RT Suhdi",
-      "Syifa",
-      "Paman Abu"
+      "Pemain B1",
+      "Pemain B2",
+      "Pemain B3",
+      "Pemain B4",
+      "Pemain B5",
+      "Pemain B6",
+      "Pemain B7",
+      "Pemain B8",
+      "Pemain B9",
+      "Pemain B10"
+    ]
+  },
+  {
+    nama: "Tanjung Sari 1B",
+    pemain: [
+      "Pemain 1B1",
+      "Pemain 1B2",
+      "Pemain 1B3",
+      "Pemain 1B4",
+      "Pemain 1B5",
+      "Pemain 1B6",
+      "Pemain 1B7",
+      "Pemain 1B8",
+      "Pemain 1B9",
+      "Pemain 1B10"
+    ]
+  },
+  {
+    nama: "Tanjung Sari 2B",
+    pemain: [
+      "Pemain 2B1",
+      "Pemain 2B2",
+      "Pemain 2B3",
+      "Pemain 2B4",
+      "Pemain 2B5",
+      "Pemain 2B6",
+      "Pemain 2B7",
+      "Pemain 2B8",
+      "Pemain 2B9",
+      "Pemain 2B10"
     ]
   }
 ];
+
 window.teams = teams;
