65e443563459657c3476135dd4ce33eb4212ef99
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-29 20:55:20 +0700

    Update tim.js

--- supercup/tim.js
+++ supercup/tim.js
@@ -1,5 +1,23 @@
 // tim.js
 const teams = [
+  // =======================
+  // Grup A
+  // =======================
+  {
+    nama: "Tanjung Sari 1B",
+    pemain: [
+      "Pemain 1B1",
+      "Pemain 1B2",
+      "Pemain 1B3",
+      "Pemain 1B4",
+      "Pemain 1B5",
+      "Pemain 1B6",
+      "Pemain 1B7",
+      "Pemain 1B8",
+      "Pemain 1B9",
+      "Pemain 1B10"
+    ]
+  },
   {
     nama: "Tanjung Bulan A",
     pemain: [
@@ -16,6 +34,25 @@ const teams = [
     ]
   },
   {
+    nama: "Tanjung Sari 2B",
+    pemain: [
+      "Pemain 2B1",
+      "Pemain 2B2",
+      "Pemain 2B3",
+      "Pemain 2B4",
+      "Pemain 2B5",
+      "Pemain 2B6",
+      "Pemain 2B7",
+      "Pemain 2B8",
+      "Pemain 2B9",
+      "Pemain 2B10"
+    ]
+  },
+
+  // =======================
+  // Grup B
+  // =======================
+  {
     nama: "Tanjung Sari 1A",
     pemain: [
       "Pemain 1A1",
@@ -28,21 +65,6 @@ const teams = [
       "Pemain 1A8",
       "Pemain 1A9",
       "Pemain 1A10"
-    ]
-  },
-  {
-    nama: "Tanjung Sari 2A",
-    pemain: [
-      "Pemain 2A1",
-      "Pemain 2A2",
-      "Pemain 2A3",
-      "Pemain 2A4",
-      "Pemain 2A5",
-      "Pemain 2A6",
-      "Pemain 2A7",
-      "Pemain 2A8",
-      "Pemain 2A9",
-      "Pemain 2A10"
     ]
   },
   {
@@ -61,33 +83,18 @@ const teams = [
     ]
   },
   {
-    nama: "Tanjung Sari 1B",
+    nama: "Tanjung Sari 2A",
     pemain: [
-      "Pemain 1B1",
-      "Pemain 1B2",
-      "Pemain 1B3",
-      "Pemain 1B4",
-      "Pemain 1B5",
-      "Pemain 1B6",
-      "Pemain 1B7",
-      "Pemain 1B8",
-      "Pemain 1B9",
-      "Pemain 1B10"
-    ]
-  },
-  {
-    nama: "Tanjung Sari 2B",
-    pemain: [
-      "Pemain 2B1",
-      "Pemain 2B2",
-      "Pemain 2B3",
-      "Pemain 2B4",
-      "Pemain 2B5",
-      "Pemain 2B6",
-      "Pemain 2B7",
-      "Pemain 2B8",
-      "Pemain 2B9",
-      "Pemain 2B10"
+      "Pemain 2A1",
+      "Pemain 2A2",
+      "Pemain 2A3",
+      "Pemain 2A4",
+      "Pemain 2A5",
+      "Pemain 2A6",
+      "Pemain 2A7",
+      "Pemain 2A8",
+      "Pemain 2A9",
+      "Pemain 2A10"
     ]
   }
 ];
