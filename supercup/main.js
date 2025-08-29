4341bdce4073798df441badf1e204e2dc144c7c1
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-29 21:01:24 +0700

    Update main.js

--- supercup/main.js
+++ supercup/main.js
@@ -1,17 +1,17 @@
 // main.js
-// Jadwal: 30 Agustus - 1 September 2025, 2 pertandingan per hari (Grup A & B)
+// Jadwal: 31 Agustus - 2 September 2025, 2 pertandingan per hari (Grup A & B)
 const matches = [
-  // 30 Agustus 2025
-  { home: "Tanjung Bulan A", away: "Tanjung Sari 1A" },
-  { home: "Tanjung Bulan B", away: "Tanjung Sari 1B" },
-
   // 31 Agustus 2025
-  { home: "Tanjung Bulan A", away: "Tanjung Sari 2A" },
-  { home: "Tanjung Bulan B", away: "Tanjung Sari 2B" },
+  { home: "Tanjung Sari 1B", away: "Tanjung Sari 1A" },
+  { home: "Tanjung Bulan A", away: "Tanjung Bulan B" },
 
   // 1 September 2025
-  { home: "Tanjung Sari 1A", away: "Tanjung Sari 2A" },
-  { home: "Tanjung Sari 1B", away: "Tanjung Sari 2B" }
+  { home: "Tanjung Sari 2B", away: "Tanjung Sari 2A" },
+  { home: "Tanjung Sari 1B", away: "Tanjung Bulan A" },
+
+  // 2 September 2025
+  { home: "Tanjung Sari 1A", away: "Tanjung Bulan B" },
+  { home: "Tanjung Bulan A", away: "Tanjung Sari 2B" }
 ];
 
 // Kalau nanti ingin menambahkan skor, tambahkan properti setHome dan setAway:
