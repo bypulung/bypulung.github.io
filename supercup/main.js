30f8a0a72dd3e8f265f08b82daf40fb0181b61fa
bypulung <106810287+bypulung@users.noreply.github.com>
2025-08-29 21:44:14 +0700

    Update main.js

--- supercup/main.js
+++ supercup/main.js
@@ -1,20 +1,21 @@
 // main.js
-// Jadwal: 31 Agustus - 2 September 2025, 2 pertandingan per hari (Grup A & B)
+// Jadwal: 31 Agustus - 2 September 2025, Grup A & Grup B
+
 const matches = [
   // 31 Agustus 2025
-  { home: "Tanjung Sari 1B", away: "Tanjung Sari 1A" },
-  { home: "Tanjung Bulan A", away: "Tanjung Bulan B" },
+  { home: "Tanjung Sari 1 B", away: "Tanjung Bulan A", setHome: 0, setAway: 0 },
+  { home: "Tanjung Sari 2A", away: "Tanjung Bulan B", setHome: 0, setAway: 0 },
 
   // 1 September 2025
-  { home: "Tanjung Sari 2B", away: "Tanjung Sari 2A" },
-  { home: "Tanjung Sari 1B", away: "Tanjung Bulan A" },
+  { home: "Tanjung Bulan A", away: "Tanjung Sari 2B", setHome: 0, setAway: 0 },
+  { home: "Tanjung Bulan B", away: "Tanjung Sari 1A", setHome: 0, setAway: 0 },
 
   // 2 September 2025
-  { home: "Tanjung Sari 1A", away: "Tanjung Bulan B" },
-  { home: "Tanjung Bulan A", away: "Tanjung Sari 2B" }
+  { home: "Tanjung Sari 1 B", away: "Tanjung Sari 2B", setHome: 0, setAway: 0 },
+  { home: "Tanjung Sari 2A", away: "Tanjung Sari 1A", setHome: 0, setAway: 0 }
 ];
 
-// Kalau nanti ingin menambahkan skor, tambahkan properti setHome dan setAway:
-// contoh: { home: "Tanjung Bulan A", away: "Tanjung Sari 1A", setHome: 2, setAway: 1 }
+// Kalau nanti ingin menambahkan skor, tinggal isi properti setHome dan setAway
+// contoh: { home: "Tanjung Bulan A", away: "Tanjung Sari 2B", setHome: 2, setAway: 1 }
 
 window.matches = matches;
